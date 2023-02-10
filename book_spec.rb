67360c8ee44287a5f310fb0bdc9b1780e0090e56
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-10 14:33:21 +0100

    added test scenarion for no format support

--- book_spec.rb
+++ book_spec.rb
@@ -13,4 +13,8 @@ describe "Book" do
     end
     expect(Book.new("Le Petit Prince", "Exupery").return_book_in('csv')).to eq expected
   end
+
+  it "return_book_in should return a csv string with when passed argument 'csv'" do
+    expect(Book.new("Alchimist", "Paolo").return_book_in("pdf")).to eq "Error:Format not supported"
+  end
 end
