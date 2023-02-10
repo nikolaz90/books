0023c154b570752cf96e22465afcbd585d002362
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-10 14:20:27 +0100

    added books and book spec

---
+++ book.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'csv'
+
+class Book
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+  def return_book_collection(format_required)
+    case format_required
+    when "csv"
+      return csv_response
+    when 'json'
+      return json_response
+    end
+  end
+
+  private
+
+  def json_response
+    { title: @title, author: @author }.to_json
+  end
+
+  def csv_response
+    CSV.generate do |csv|
+      csv << ["Book title", "Author"]
+      csv << [@title, @author]
+    end
+  end
+end
